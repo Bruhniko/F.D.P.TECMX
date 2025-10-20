d0db87eeba44138e950ec0a12e778b75406ab003
JESSIEXO <nikolinelv649@gmail.com>
2025-10-20 07:47:13 -0600

    created Encapsulamiento

---
+++ Unit #3/Encapsulamiento.py
@@ -0,0 +1,43 @@
+class Auto:
+    def __init__(self, marca, velocidad_maxima):
+        self.__marca = marca           # atributo privado
+        self.__velocidad_maxima = velocidad_maxima
+        self.__velocidad_actual = 0
+
+    # obtiene velocidad
+    # método getter
+    def get_velocidad_actual(self):
+        return self.__velocidad_actual
+
+    # método para acelerar
+    # aumenta la velocidad
+    def acelerar(self, aumento):
+        if self.__velocidad_actual + aumento > self.__velocidad_maxima:
+            self.__velocidad_actual = self.__velocidad_maxima
+        else:
+            self.__velocidad_actual += aumento
+
+    # método para frenar
+    # disminuye la velocidad
+    def frenar(self, reduccion):
+        if self.__velocidad_actual - reduccion < 0:
+            self.__velocidad_actual = 0
+        else:
+            self.__velocidad_actual -= reduccion
+
+
+auto1 = Auto("Toyota", 180)
+auto2 = Auto("Honda", 200)
+
+auto1.acelerar(50)
+print("Velocidad actual:", auto1.get_velocidad_actual(), "km/h")
+
+auto1.acelerar(200)
+print("Velocidad actual:", auto1.get_velocidad_actual(), "km/h")
+
+auto1.frenar(100)
+print("Velocidad actual:", auto1.get_velocidad_actual(), "km/h")
+
+# __marca crea una variable privada
+# -marca variables que se mandan a llamar primero a la clase
+# marca variable publica
